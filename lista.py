068141847e7ce6be319a6cb505dfbfc3d54908ea
GiandomenicoIameo <giandomenicoiameo00@gmail.com>
2019-12-13 23:20:44 +0100

    lista.py

---
+++ lista.py
@@ -0,0 +1,21 @@
+# Lista
+# Si scriva un programma Python che, data una lista
+# di valori qualsiasi, ne stampi gli elementi uno per riga.
+
+# Esempio di output:
+
+# Ho memorizzato la lista seguente:
+# [1, 2, 3, 'hello', 4.2, (0, 0)]
+
+# Gli elementi della lista, uno per riga, sono:
+# 1
+# 2
+# 3
+# 'hello'
+# 4.2
+# (0, 0)
+
+lista = [ 1, 2, 3, "hello", 4.2, 0 ]
+
+for elementi in lista:
+    print( elementi )

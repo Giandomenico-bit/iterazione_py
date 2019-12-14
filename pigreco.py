6c9a35f52f7c1c6c26f1201430e690873ac615f5
GiandomenicoIameo <giandomenicoiameo00@gmail.com>
2019-12-15 00:06:06 +0100

    pigreco.py

---
+++ pigreco.py
@@ -0,0 +1,36 @@
+# PiGreco
+# Si scriva un programma in Python che calcoli il valore di π mediante la formula:
+
+# π = 4 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + ...
+# Il numero di termini della serie da utilizzare deve essere chiesto all'utente.
+
+# Esempio di esecuzione:
+
+# Approssimazione di Pi Greco
+# mediante la serie:
+
+# 4 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + ...
+
+# Quanti termini vuoi utilizzare? 5
+# PI: 3.33968253968
+
+termini = input( "Inserisci numero termini : " )
+
+termini = int( termini )
+pigreco = 0.0
+i = 1
+count = 0
+potenza = -1
+esponente = -1
+
+while count != termini :
+
+    if i % 2 != 0 :
+        potenza = potenza * esponente
+        termine = 4.0 / float( i )
+        termine = termine * potenza
+        pigreco = pigreco + termine
+        count = count + 1
+    i = i + 1
+
+print( pigreco )
